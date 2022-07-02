999566f22b283d37e9fccf69b6e6e96e83ffb58e
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-07-01 22:03:22 -0500

    cambiar el tamaño del pincel

--- js/index.js
+++ js/index.js
@@ -6,8 +6,33 @@ $(document).ready(start);
 var draw; //es falsa
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
+var radius = 10;
+var minRadius = 2;
+var maxRadius = 30;
+
+function pincelSize(){
+  //asignando los tamaños del pincel
+  $('#increase').click(function(){
+    //aumenta el tamaño del pincel cuando se de click
+    radius = radius + 2;
+    if (radius >= maxRadius) {
+      radius = maxRadius;
+    }
+    $('#pincelval').text(radius);
+  });
+
+  $('#decrease').click(function() {
+    //disminuye el tamaño del pincel cuando se de click
+    radius = radius - 2;
+    if (radius <= minRadius) {
+      radius = minRadius;
+    }
+    $('#pincelval').text(radius);
+  });
+}
 
 function start(){
+  pincelSize();
   //seteando el tamaño de canvas
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
@@ -25,7 +50,7 @@ function start(){
   function paint(){
     if (draw){
       //para que el ancho de la linea sea igual que al de los puntos(círculos)
-      context.lineWidth = 10 * 2
+      context.lineWidth = radius * 2
       //punto de inicio
       context.lineTo(event.pageX, event.pageY);//inicio del cursor
       context.stroke();//pinta la línea
@@ -34,7 +59,7 @@ function start(){
       context.beginPath();
                                           //radio, punto donde empieza y termina en grados(1π = 180, 2π = 360)
       /* context.arc(event.pageX, event.pageY, 10, 0, 360); */
-      context.arc(event.pageX, event.pageY, 10, 0, 2*Math.PI);
+      context.arc(event.pageX, event.pageY, radius, 0, 2*Math.PI);
       context.fill(); //rellena el círculo
 
       //posicion del ultimo punto que se pinto
