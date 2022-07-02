573644eebcb769b39ab382d02128db19ae76dafb
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-07-01 21:15:29 -0500

    uniendo los puntos

--- js/index.js
+++ js/index.js
@@ -24,13 +24,22 @@ function start(){
 
   function paint(){
     if (draw){
+      //para que el ancho de la linea sea igual que al de los puntos(círculos)
+      context.lineWidth = 10 * 2
       //punto de inicio
-      context.beginPath();
+      context.lineTo(event.pageX, event.pageY);//inicio del cursor
+      context.stroke();//pinta la línea
+
       //pinta el arco en la posicion del cursor
+      context.beginPath();
                                           //radio, punto donde empieza y termina en grados(1π = 180, 2π = 360)
       /* context.arc(event.pageX, event.pageY, 10, 0, 360); */
       context.arc(event.pageX, event.pageY, 10, 0, 2*Math.PI);
       context.fill(); //rellena el círculo
+
+      //posicion del ultimo punto que se pinto
+      context.beginPath();
+      context.moveTo(event.pageX, event.pageY); // posicion del mouse
     }
   }
 
